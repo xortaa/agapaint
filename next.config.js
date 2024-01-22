2e457bc0a2c250053fffa892dfaeafbff9b46e04
xortaa <kercwinpaul@gmail.com>
2024-01-22 21:57:58 +0800

    cloudinary and upload buttons and image preview complete

--- next.config.js
+++ next.config.js
@@ -14,6 +14,12 @@ const nextConfig = {
         port: "",
         pathname: "**",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "**",
+      },
     ],
   },
 }
