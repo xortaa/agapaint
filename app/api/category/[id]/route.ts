e4127024b4f7930388edf0980be9f79875e6f65a
xortaa <kercwinpaul@gmail.com>
2024-01-28 19:44:32 +0800

    patch category

--- app/api/category/[id]/route.ts
+++ app/api/category/[id]/route.ts
@@ -13,3 +13,19 @@ export const GET = async (req: NextRequest, { params }: {params: {id: string}}) 
     return NextResponse.json("Can't find category", {status: 500})
   }
 };
+
+// UPDATE
+// plain js
+
+export const PATCH = async (req: NextRequest, {params}: {params: {id: string}}) => { 
+  const id = params.id;
+  const {name} = await req.json();
+  try {
+    await connectToDatabase()
+    const updatedCategory = await Category.findByIdAndUpdate(id, {name}, {new: true})
+    return NextResponse.json(updatedCategory, {status: 200})
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json("Can't update category", {status: 500})
+  }
+}
