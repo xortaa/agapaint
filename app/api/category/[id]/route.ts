56cd0285d39d7c65f1c85b4df150e60006b26495
xortaa <kercwinpaul@gmail.com>
2024-01-28 19:50:44 +0800

    delete category

--- app/api/category/[id]/route.ts
+++ app/api/category/[id]/route.ts
@@ -1,31 +1,46 @@
 import connectToDatabase from "@/utils/database";
 import { NextRequest, NextResponse } from "next/server";
 import Category from "@/models/category";
+import CategoryDropDown from "@/components/categories/CategoryDropDown";
 
-export const GET = async (req: NextRequest, { params }: {params: {id: string}}) => {
+export const GET = async (req: NextRequest, { params }: { params: { id: string } }) => {
   const id = params.id;
   try {
-    await connectToDatabase()
-    const category = await Category.findById(id)
-    return NextResponse.json(category, {status: 200})
+    await connectToDatabase();
+    const category = await Category.findById(id);
+    return NextResponse.json(category, { status: 200 });
   } catch (error) {
-    console.log(error)
-    return NextResponse.json("Can't find category", {status: 500})
+    console.log(error);
+    return NextResponse.json("Can't find category", { status: 500 });
   }
 };
 
 // UPDATE
 // plain js
 
-export const PATCH = async (req: NextRequest, {params}: {params: {id: string}}) => { 
+export const PATCH = async (req: NextRequest, { params }: { params: { id: string } }) => {
   const id = params.id;
-  const {name} = await req.json();
+  const { name } = await req.json();
   try {
-    await connectToDatabase()
-    const updatedCategory = await Category.findByIdAndUpdate(id, {name}, {new: true})
-    return NextResponse.json(updatedCategory, {status: 200})
+    await connectToDatabase();
+    const updatedCategory = await Category.findByIdAndUpdate(id, { name }, { new: true });
+    return NextResponse.json(updatedCategory, { status: 200 });
   } catch (error) {
-    console.log(error)
-    return NextResponse.json("Can't update category", {status: 500})
+    console.log(error);
+    return NextResponse.json("Can't update category", { status: 500 });
   }
-}
+};
+
+// DELETE
+
+export const DELETE = async (req: NextRequest, { params }: { params: { id: string } }) => {
+  const id = params.id;
+  try {
+    await connectToDatabase();
+    const deletedCategory = await Category.findByIdAndDelete(id);
+    return NextResponse.json(deletedCategory, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json("Can't delete category", { status: 500 });
+  }
+};
