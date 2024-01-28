5983916461e928a2e31cb3bca17bf931a77c282a
xortaa <kercwinpaul@gmail.com>
2024-01-28 19:12:56 +0800

    post

--- app/api/category/route.ts
+++ app/api/category/route.ts
@@ -3,16 +3,34 @@ import Category from "@/models/category"
 import { NextRequest, NextResponse } from "next/server"
 
 //get
-export const GET = async (req: NextRequest, { params }: { params: { id: string } }) => {
-  const id = params.id;
+// localhost:3000/api/category/[someething]
+// get all 
+export const GET = async (req: NextRequest) => {
   try {
     await connectToDatabase();
-    const category = await Category.findById(id);
-    console.log(category); // log the category
-    return NextResponse.json(category, { status: 200 });
+    const categories = await Category.find({});
+    console.log(categories); // log the categories
+    return NextResponse.json(categories, { status: 200 });
   } catch (error) {
     console.log(error);
     return NextResponse.json("Can't find category", { status: 500 });
   }
 };
+
+export const POST = async (req: NextRequest) => {
+  // getting the name from the json { "name": "paint"}
+  const { name } = await req.json();
+  try {
+    await connectToDatabase();
+    // initialize what the category looks like following the model
+    const newCategory = new Category({ name });
+    // saves the category
+    const savedCategory = await newCategory.save();
+    console.log(savedCategory);
+    return NextResponse.json(savedCategory, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json("Can't create category", { status: 500 });
+  }
+}
 
