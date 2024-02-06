bb0a48c0ff07a2a2b1df44801a73175690ba35a4
Moonie <iellemizuki@gmail.com>
2024-02-06 17:21:17 +0800

    revenuue-route-model

---
+++ models/revenue.ts
@@ -0,0 +1,21 @@
+import { Schema, model, models } from "mongoose";
+const revenueSchema = new Schema({
+    service: {
+        type: Schema.Types.ObjectId,
+        ref: 'Service',
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    paymentStatus: {
+        type: String,
+        enum: ['paid', 'unpaid'],
+        default: 'unpaid',
+        required: true
+    }
+});
+
+const Revenue = models.Revenue || model('Revenue', revenueSchema);
+export default Revenue;
