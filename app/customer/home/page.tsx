73fd16aaf9c27e838f4a169a619568dbd43b6879
Francine San Jose <franzsjj@gmail.com>
2024-02-20 23:26:21 +0800

    customer homepage buttons (di na blue), squares (faq fixed, responsive to diff screen sizes)

--- app/customer/home/page.tsx
+++ app/customer/home/page.tsx
@@ -21,46 +21,34 @@ function CustHome() {
         <Container>
           <Row>
             {/* View our services */}
-            <Col lg={4} className={`mb-4 mb-lg-0 ${custhomeStyles.squares}`}>
-              <Card className="bg-dark text-white rounded-card">
+            <Col lg={4} md={4} s={12} xs={12} className={custhomeStyles.squares}>
+              <Card className="bg-dark text-white rounded-card d-flex flex-column aspect-1x1">
                 <Card.Img src="../assets/img/servicesbg.png" alt="Card image" />
                 <Card.ImgOverlay className="d-flex flex-column justify-content-end">
-                  <Card.Text className={`mb-0 ${custhomeStyles.cardText}`}>
-                    View our
-                  </Card.Text>
-                  <Card.Title className={`mt-0 ${custhomeStyles.cardTitle}`}>
-                    SERVICES
-                  </Card.Title>
+                  <Card.Text className={`mb-0 ${custhomeStyles.cardText}`}>View our</Card.Text>
+                  <Card.Title className={`mt-0 ${custhomeStyles.cardTitle}`}>SERVICES</Card.Title>
                 </Card.ImgOverlay>
               </Card>
             </Col>
 
             {/* Book an appointment */}
-            <Col lg={4} className={`mb-4 mb-lg-0 ${custhomeStyles.squares}`}>
-              <Card className="bg-dark text-white d-flex flex-column">
+            <Col lg={4} md={4} s={12} xs={12} className={custhomeStyles.squares}>
+              <Card className="bg-dark text-white rounded-card d-flex flex-column aspect-1x1">
                 <Card.Img src="../assets/img//appointmentbg.png" alt="Card image" />
                 <Card.ImgOverlay className="d-flex flex-column justify-content-end">
-                  <Card.Text className={`mb-0 ${custhomeStyles.cardText}`}>
-                    Book an
-                  </Card.Text>
-                  <Card.Title className={`mt-0 ${custhomeStyles.cardTitle}`}>
-                    APPOINTMENT
-                  </Card.Title>
+                  <Card.Text className={`mb-0 ${custhomeStyles.cardText}`}>Book an</Card.Text>
+                  <Card.Title className={`mt-0 ${custhomeStyles.cardTitle}`}>APPOINTMENT</Card.Title>
                 </Card.ImgOverlay>
               </Card>
             </Col>
 
-            {/* Read the FAQ */}
-            <Col lg={4} className={`mb-4 mb-lg-0 ${custhomeStyles.squares}`}>
-              <Card className="bg-dark text-white">
-                <Card.Img src="../assets/img//faqbg.png" alt="Card image" />
+            {/* Read the faq */}
+            <Col lg={4} md={4} s={12} xs={12} className={custhomeStyles.squares}>
+              <Card className="bg-dark text-white rounded-card d-flex flex-column aspect-1x1">
+                <Card.Img src="../assets/img//fraqbg.png" alt="Card image" />
                 <Card.ImgOverlay className="d-flex flex-column justify-content-end">
-                  <Card.Text className={`mb-0 ${custhomeStyles.cardText}`}>
-                    Read the
-                  </Card.Text>
-                  <Card.Title className={`mt-0 ${custhomeStyles.cardTitle}`}>
-                    FAQ
-                  </Card.Title>
+                  <Card.Text className={`mb-0 ${custhomeStyles.cardText}`}>Read the</Card.Text>
+                  <Card.Title className={`mt-0 ${custhomeStyles.cardTitle}`}>FAQ</Card.Title>
                 </Card.ImgOverlay>
               </Card>
             </Col>
@@ -80,24 +68,18 @@ function CustHome() {
               {/* first row */}
               <Row className="mb-3">
                 <Col lg={6} className="order-md-1 mb-3">
-                  <Image
-                    src={about1}
-                    alt="about1"
-                    className={custhomeStyles.img}
-                  />
+                  <Image src={about1} alt="about1" className={custhomeStyles.img} />
                 </Col>
 
                 <Col lg={6} className="order-md-2 order-lg-2 order-sm-2 order-xs-2 mb-3">
                   <p className={custhomeStyles.leftP}>
-                    AGAPAINT, established in 2020, is a shop that specialized in
-                    Automotive Detailing, Ceramic & Graphene Coating, and Body
-                    Repair and Repaint Services using the highest quality
-                    products, tools, and materials.
-                    <br /><br />
-
-                    AGAPAINT, earned a solid reputation for excellence and
-                    reliable workmanship began as a small one-service shop and
-                    has now grown to become one of the most trusted names.
+                    AGAPAINT, established in 2020, is a shop that specialized in Automotive Detailing, Ceramic &
+                    Graphene Coating, and Body Repair and Repaint Services using the highest quality products, tools,
+                    and materials.
+                    <br />
+                    <br />
+                    AGAPAINT, earned a solid reputation for excellence and reliable workmanship began as a small
+                    one-service shop and has now grown to become one of the most trusted names.
                   </p>
                 </Col>
               </Row>
@@ -106,23 +88,16 @@ function CustHome() {
               <Row className="mb-5">
                 <Col lg={6} className="order-md-2 order-lg-1">
                   <p className={custhomeStyles.rightP}>
-                    AGAPAINT provides an automotive service with multiple
-                    packages for a variety of vehicles. Whether you only need
-                    your interior done or only a basic detail, focused on
-                    developing and improving painting, detailing, and ceramic
-                    coating services, whether you only need your interior done
-                    or only a basic detail, focused on developing and improving
-                    painting, detailing, and ceramic coating services.
-                    
+                    AGAPAINT provides an automotive service with multiple packages for a variety of vehicles. Whether
+                    you only need your interior done or only a basic detail, focused on developing and improving
+                    painting, detailing, and ceramic coating services, whether you only need your interior done or only
+                    a basic detail, focused on developing and improving painting, detailing, and ceramic coating
+                    services.
                   </p>
                 </Col>
 
                 <Col lg={6} className="order-md-1 order-lg-2 order-sm-1 order-xs-1">
-                  <Image
-                    src={about2}
-                    alt="about1"
-                    className={custhomeStyles.img}
-                  />
+                  <Image src={about2} alt="about1" className={custhomeStyles.img} />
                 </Col>
               </Row>
             </Container>
