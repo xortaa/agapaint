1b66c93c72d8e8a1af53ff4dfde715b9b7544c21
xortaa <kercwinpaul@gmail.com>
2024-02-22 09:17:23 +0800

    prettier

--- models/log.ts
+++ models/log.ts
@@ -1,39 +1,39 @@
 import { Schema, model, models } from "mongoose";
 const logSchema = new Schema({
-    material: { 
-        type: Schema.Types.ObjectId, 
-        ref: 'Material',
-        required: true
-    },
-    transactionType: { 
-        type: String, 
-        required: true 
-    },
-    quantity: { 
-        type: Number, 
-        required: true 
-    },
-    date: { 
-        type: Date, 
-        required: true 
-    },
-    notes: { 
-        type: String 
-    },
-    updateDate: { 
-        type: Date, 
-        default: Date.now,
-        required: true
-    },
-    updatedBy: { 
-        type: String,
-        required: true
-    },
-    isArchived: { 
-        type: Boolean, 
-        default: false 
-    }
+  material: {
+    type: Schema.Types.ObjectId,
+    ref: "Material",
+    required: true,
+  },
+  transactionType: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  updateDate: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  updatedBy: {
+    type: String,
+    required: true,
+  },
+  isArchived: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-const Log = models.Log || model('Log', logSchema);
+const Log = models.Log || model("Log", logSchema);
 export default Log;
