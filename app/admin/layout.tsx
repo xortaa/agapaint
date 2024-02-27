e54d7aabff4c8867cf4b9b0e08aa9ed3717830e2
Kaiehla <espiritu.kaiehla@gmail.com>
2024-02-27 15:46:24 +0800

    fixed children padding to 0

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -15,7 +15,7 @@ const AdminLayout = ({ children }) => {
       </div>
 
       {/* for the children or the main content */}
-      <div style={{ width: contentWidth, overflowY: "auto", padding: "8px 0px 8px 10px" }}>{children}</div>
+      <div style={{ width: contentWidth, overflowY: "auto", padding: "0px" }}>{children}</div>
     </div>
   );
 };
