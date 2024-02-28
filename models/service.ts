409d960886564bac8235a6a02cb139fa3090282e
xortaa <kercwinpaul@gmail.com>
2024-02-29 06:48:34 +0800

    upload-complete

--- models/service.ts
+++ models/service.ts
@@ -1,30 +1,25 @@
 import { Schema, model, models } from "mongoose";
 
 const ServiceSchema = new Schema({
-  // service name
   name: {
     type: String,
     required: true,
-    unique: true,
   },
-
-  //service description
   description: {
     type: String,
     required: true,
-    unique: true,
   },
-
-  //service image
   image: {
     type: String,
     required: true,
   },
-
   price: {
     type: Number,
     required: true,
     default: 0,
+  },
+  carType: {
+    type: String,
   },
 });
 
