0fdd042c4dafae253bdadc2c7f4487f0ab391483
xortaa <kercwinpaul@gmail.com>
2024-03-28 14:01:44 +0800

    redirection authentication middleware for client side and admin side

--- middleware.ts
+++ middleware.ts
@@ -3,16 +3,31 @@ import { NextRequest, NextResponse } from "next/server";
 
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/admin/hub/:path*"] };
+export const config = {
+  matcher: ["/admin/signup", "/admin/hub/:path*", "/customer/appointment", "/customer/appointment/payment", "/booking"],
+};
 
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
 
-  if (token && token.email !== process.env.ADMIN_EMAIL) {
-    return NextResponse.redirect(process.env.NEXTAUTH_URL);
+  if (request.nextUrl.pathname === "/admin/signup" && token && token.email !== process.env.ADMIN_EMAIL) {
+    return NextResponse.redirect(`${process.env.NEXTAUTH_URL}`);
   }
 
-  if (!token) {
+  if (request.nextUrl.pathname.startsWith("/admin/hub") && token && token.email !== process.env.ADMIN_EMAIL) {
+    return NextResponse.redirect(`${process.env.NEXTAUTH_URL}`);
+  }
+
+  if (request.nextUrl.pathname.startsWith("/admin/hub") && (!token || token.email !== process.env.ADMIN_EMAIL)) {
     return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/admin/signup`);
   }
+
+  if (
+    !token &&
+    (request.nextUrl.pathname.startsWith("/customer/appointment") || request.nextUrl.pathname === "/booking")
+  ) {
+    return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/customer/signup`);
+  }
+
+  return NextResponse.next();
 }
