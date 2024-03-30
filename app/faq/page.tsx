b26c397abc59749bf8b3fe44a94158b1cbe42381
kaiehla <espiritu.kaiehla@gmail.com>
2024-03-30 19:24:14 +0800

    fix design bugs in faq customer

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -4,7 +4,7 @@
 import faqStyles from "@/styles/faq.module.scss";
 
 //bootsrap
-import { Container, Accordion, Row } from "react-bootstrap";
+import { Container, Accordion, Row, Col } from "react-bootstrap";
 
 import { useState, useEffect } from "react";
 import { getSession } from "next-auth/react";
@@ -31,7 +31,7 @@ const page = () => {
       });
     };
     fetchFaqs();
-    checkSignInStatus().then(isSignedIn => setIsSignedIn(isSignedIn));
+    checkSignInStatus().then((isSignedIn) => setIsSignedIn(isSignedIn));
   }, []);
 
   return (
@@ -40,16 +40,20 @@ const page = () => {
         {/*    navbar here */}
         {isSignedIn ? <Navbar2 /> : <Navbar />}
         <h1 className={faqStyles.faqtitle}>Frequently Asked Questions</h1>
-      </Container>
 
-      <Accordion>
-        {faqs.map((faq) => (
-          <Accordion.Item className={faqStyles.card} eventKey={faq._id} key={faq._id}>
-            <Accordion.Header className={faqStyles.cardtitle}>{faq.question}</Accordion.Header>
-            <Accordion.Body className={faqStyles.cardbody}>{faq.answer}</Accordion.Body>
-          </Accordion.Item>
-        ))}
-      </Accordion>
+        <Row className="justify-content-center">
+          <Col xl={10} xs={11}>
+            <Accordion alwaysOpen>
+              {faqs.map((faq) => (
+                <Accordion.Item className={faqStyles.card} eventKey={faq._id} key={faq._id}>
+                  <Accordion.Header className={faqStyles.cardtitle}>{faq.question}</Accordion.Header>
+                  <Accordion.Body className={faqStyles.cardbody}>{faq.answer}</Accordion.Body>
+                </Accordion.Item>
+              ))}
+            </Accordion>
+          </Col>
+        </Row>
+      </Container>
 
       {/* footer */}
     </main>
