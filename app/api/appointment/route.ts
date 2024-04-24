ef3774fc8cad8c46e8fb34e4f31f82fb309e7d3e
xortaa <kercwinpaul@gmail.com>
2024-04-24 19:04:32 +0800

    removed recaptcha on all booking related

--- app/api/appointment/route.ts
+++ app/api/appointment/route.ts
@@ -8,43 +8,31 @@ import axios from "axios";
 
 export const POST = async (req: NextRequest) => {
   const appointmentData = await req.json();
-  const { recaptchaResponse } = appointmentData;
   try {
     await connectToDatabase();
-
-    const response = await axios.post("https://www.google.com/recaptcha/api/siteverify", null, {
-      params: {
-        secret: process.env.RECAPTCHA_SECRET_KEY,
-        response: recaptchaResponse,
-      },
-    });
-
-    if (response.data.success) {
-      if (appointmentData.paymentTerm === "Partial") {
-        appointmentData.payments = [
-          { amount: parseFloat((appointmentData.totalPrice * 0.5).toFixed(2)), status: "Unpaid" },
-          { amount: parseFloat((appointmentData.totalPrice * 0.25).toFixed(2)), status: "Unpaid" },
-          { amount: parseFloat((appointmentData.totalPrice * 0.25).toFixed(2)), status: "Unpaid" },
-        ];
-      } else {
-        appointmentData.payments = [{ amount: parseFloat(appointmentData.totalPrice.toFixed(2)), status: "Unpaid" }];
-      }
-
-      const nanoid = customAlphabet("1234567890", 5);
-      appointmentData.nanoid = nanoid();
-
-      const appointment = await Appointment.create(appointmentData);
-
-      const customer = await User.findById(appointment.customerId);
-
-      customer.appointment.push(appointment._id);
-
-      await customer.save();
-
-      return NextResponse.json(appointment, { status: 200 });
+   
+    if (appointmentData.paymentTerm === "Partial") {
+      appointmentData.payments = [
+        { amount: parseFloat((appointmentData.totalPrice * 0.5).toFixed(2)), status: "Unpaid" },
+        { amount: parseFloat((appointmentData.totalPrice * 0.25).toFixed(2)), status: "Unpaid" },
+        { amount: parseFloat((appointmentData.totalPrice * 0.25).toFixed(2)), status: "Unpaid" },
+      ];
     } else {
-      return NextResponse.json("Recaptcha failed", { status: 400 });
+      appointmentData.payments = [{ amount: parseFloat(appointmentData.totalPrice.toFixed(2)), status: "Unpaid" }];
     }
+
+    const nanoid = customAlphabet("1234567890", 5);
+    appointmentData.nanoid = nanoid();
+
+    const appointment = await Appointment.create(appointmentData);
+
+    const customer = await User.findById(appointment.customerId);
+
+    customer.appointment.push(appointment._id);
+
+    await customer.save();
+
+    return NextResponse.json(appointment, { status: 200 });
   } catch (error) {
     console.log(error);
     return NextResponse.json("Can't create appointment", { status: 500 });
