fc9aa8b4be9cfd585a51c8c9b3520e10943e2d4c
xortaa <kercwinpaul@gmail.com>
2024-04-24 17:50:33 +0800

    added recaptcha to sign in

--- components/SignUpCard.tsx
+++ components/SignUpCard.tsx
@@ -2,21 +2,39 @@ import { Button, Card } from "react-bootstrap";
 import Link from "@/components/Link";
 import GoogleButton from "react-google-button";
 import { signIn } from "next-auth/react";
+import ReCAPTCHA from "react-google-recaptcha";
+import { useState } from "react";
+import { set } from "mongoose";
 
-const SignUpCard = ({role}) => {
+const SignUpCard = ({ role }) => {
+  const [token, setToken] = useState<String | null>();
+
+  const recaptchaOnchange = (token: String) => {
+    setToken(token);
+  };
+
   return (
     <section>
       {/* Google Auth Button */}
       <div className="my-4">
         <GoogleButton
-          style={{ fontFamily: "", borderRadius: "5px", color: "#000", border: "1px solid #A9A9A9" }}
+          style={{
+            fontFamily: "",
+            borderRadius: "5px",
+            color: "#000",
+            border: "1px solid #A9A9A9",
+            marginBottom: "20px",
+          }}
           className="w-100 fw-medium d-flex justify-content-center align-items-center my-google-button"
           type="light"
           onClick={() => {
             signIn("google", { callbackUrl: role === "admin" ? "/admin/hub/appointment" : "/customer/appointment" });
           }}
           label="Continue with Google"
+          disabled={!token}
         />
+
+        <ReCAPTCHA sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY} onChange={recaptchaOnchange} />
       </div>
     </section>
   );
