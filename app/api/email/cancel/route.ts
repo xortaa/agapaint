6967caee3c53c37597d0594540335e94975c31b2
xortaa <kercwinpaul@gmail.com>
2024-04-25 12:52:52 +0800

    cancelled backend

---
+++ app/api/email/cancel/route.ts
@@ -0,0 +1,58 @@
+import { type NextRequest, NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+import Mail from "nodemailer/lib/mailer";
+import { cancelled } from "@/utils/email/cancelled";
+import * as handlebars from "handlebars";
+
+export async function POST(request: NextRequest) {
+  const emailData = await request.json();
+
+  const transport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.ADMIN_EMAIL,
+      pass: process.env.ADMIN_EMAIL_PASSWORD,
+    },
+  });
+
+  const compileEmailTemplate = (
+    nanoid: string,
+    date: string,
+    time: string,
+    carType: string,
+    carManufacturer: string,
+    carModel: string,
+    url: string
+  ) => {
+    const template = handlebars.compile(cancelled);
+    const htmlBody = template({
+      nanoid: nanoid,
+      date: date,
+      time: time,
+      carType: carType,
+      carManufacturer: carManufacturer,
+      carModel: carModel,
+      url: url,
+    });
+    return htmlBody;
+  };
+
+  const mailOptions: Mail.Options = {
+    from: process.env.ADMIN_EMAIL,
+    to: emailData.email,
+    subject: `Agapaint Appointment Confirmed for ${emailData.carManufacturer} ${emailData.carModel}`,
+    html: compileEmailTemplate(
+      emailData.nanoid,
+      emailData.date,
+      emailData.time,
+      emailData.carType,
+      emailData.carManufacturer,
+      emailData.carModel,
+      emailData.url
+    ),
+  };
+
+  await transport.sendMail(mailOptions);
+
+  return NextResponse.json({ message: "Email sent" });
+}
