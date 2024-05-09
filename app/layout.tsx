6f59c522644bc5f83cfedab9775bd85c8d6ece71
xortaa <kercwinpaul@gmail.com>
2024-05-10 01:55:36 +0800

    added title metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import "./globals.css";
 import { getServerSession } from "next-auth";
 import SessionProvider from "@/components/auth/SessionProvider";
@@ -5,15 +6,19 @@ import { Next13NProgress, Link } from "nextjs13-progress";
 // Bootstrap
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 // Google font - Poppins
 import { Poppins } from "next/font/google";
 const poppins = Poppins({
-   subsets: ['latin'],
-   display: 'swap',
-   variable: '--font-poppins',
-   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
- });
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "AGAPAINT | COMMITMENT TO QUALITY",
+};
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession();
