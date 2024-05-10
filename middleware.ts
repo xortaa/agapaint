36ea84c35867eea759b38c502eb6fe46b456dd30
xortaa <kercwinpaul@gmail.com>
2024-05-10 20:41:03 +0800

    /admin will redirect to either signup or dashboard

--- middleware.ts
+++ middleware.ts
@@ -4,28 +4,36 @@ import { NextRequest, NextResponse } from "next/server";
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/admin/signup", "/admin/hub/:path*", "/customer/appointment", "/customer/appointment/payment", "/booking"],
+  matcher: [
+    "/admin",
+    "/admin/signup",
+    "/admin/hub/:path*",
+    "/customer/appointment",
+    "/customer/appointment/payment",
+    "/booking",
+  ],
 };
 
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
 
+  if (request.nextUrl.pathname === "/admin") {
+    if (!token || token.email !== process.env.ADMIN_EMAIL) {
+      return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/admin/signup`);
+    } else {
+      return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/admin/hub/dashboard`);
+    }
+  }
+
   if (request.nextUrl.pathname === "/admin/signup" && token && token.email !== process.env.ADMIN_EMAIL) {
     return NextResponse.redirect(`${process.env.NEXTAUTH_URL}`);
   }
 
-  if (
-    request.nextUrl.pathname.startsWith("/admin/hub") &&
-    token &&
-    token.email !== process.env.ADMIN_EMAIL
-  ) {
+  if (request.nextUrl.pathname.startsWith("/admin/hub") && token && token.email !== process.env.ADMIN_EMAIL) {
     return NextResponse.redirect(`${process.env.NEXTAUTH_URL}`);
   }
 
-  if (
-    request.nextUrl.pathname.startsWith("/admin/hub") &&
-    (!token || token.email !== process.env.ADMIN_EMAIL)
-  ) {
+  if (request.nextUrl.pathname.startsWith("/admin/hub") && (!token || token.email !== process.env.ADMIN_EMAIL)) {
     return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/admin/signup`);
   }
 
